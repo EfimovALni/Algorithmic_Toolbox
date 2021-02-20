3007f51ea80f660b9533fde535f36058bbc9d973
EfimovAlNi <EfimovAlNi@ya.ru>
2021-02-20 18:22:21 +0100

    Here I wrote stress-test. know it works.

--- Week_1/Task_2_1.java
+++ Week_1/Task_2_1.java
@@ -1,32 +1,21 @@
-import java.util.Random;
 import java.util.Vector;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Task_2_1 {
 
-//    public static Vector<Long> Vector(int sizeOfVector) {
-//        Vector<Long> vector = new Vector<>(sizeOfVector);                        // Create 'Vector' instead of array
-//        for (int i = 0; i < sizeOfVector; i++) {
-//            vector.add(ThreadLocalRandom.current().nextLong(1_0));        // Filling vector pseudo-random numbers
-//        }
-//        return vector;
-//    }
-
     public static long MaxPairwiseProduct(int sizeOfVector, Vector<Long> vector) {
         long result = 0;
-
-//        Vector<Long> vector = new Vector<>(sizeOfVector);                        // Create 'Vector' instead of array
-//        for (int i = 0; i < sizeOfVector; i++) {
-//            vector.add(ThreadLocalRandom.current().nextLong(1_0));        // Filling vector pseudo-random numbers
-//        }
-
-        System.out.println("Size of array: " + sizeOfVector + "\t" + vector);   // Display to console
+        long max_1 = 0;
+        long max_2 = 0;
 
         for (int i = 0; i < sizeOfVector; i++) {
             for (int j = i + 1; j < sizeOfVector; j++) {
-                long tempResult = vector.get(i) * vector.get(j);                // Find two maximum numbers by multiplying
-                if (tempResult > result) {
-                    result = tempResult;
+                max_1 = vector.get(i);
+                if (vector.get(i) != vector.get(j) && (vector.get(j) != max_1)) {
+                    long tempResult = vector.get(i) * vector.get(j);                // Find two maximum numbers by multiplying
+                    if (tempResult > result) {
+                        result = tempResult;
+                    }
                 }
             }
         }
@@ -35,41 +24,56 @@ public class Task_2_1 {
 
 
     public static long MaxPairwiseProductFast(int sizeOfVector, Vector<Long> vector) {
-        int maxIndex_1 = 0;
-        int maxIndex_2 = 0;
-
-        System.out.println("Size of array: " + sizeOfVector + "\t" + vector);   // Display to console
+        long max_1 = 0;
+        long max_2 = 0;
 
 //        Find first maximum number
         for (int i = 0; i < sizeOfVector; i++) {
-            if (vector.get(i) > maxIndex_1) {
-                maxIndex_1 = Math.toIntExact(vector.get(i));
+            if (vector.get(i) > max_1) {
+                max_1 = vector.get(i);
             }
         }
 
 //        Find second maximum number
         for (int i = 0; i < sizeOfVector; i++) {
-            if ((vector.get(i) > maxIndex_2) && (vector.get(i) != maxIndex_1)) {
-                maxIndex_2 = Math.toIntExact(vector.get(i));
+            if ((vector.get(i) != max_1) && (vector.get(i) > max_2)) {
+                max_2 = vector.get(i);
             }
         }
-        return maxIndex_2 * maxIndex_1 ;
+
+        return max_2 * max_1;
     }
 
 
     public static void main(String[] args) {
-        Random sizeOfVectorGenerate = new Random();
-        int sizeOfVector = sizeOfVectorGenerate.nextInt(13);
+        int max = 13;
+        int min = 2;
+        int step = 0;
+
+        while (true) {
+            int sizeOfVector = (int) ((Math.random() * (max - min)) + min);         // Generate & calculate numbers of interval [2..13]
 
 //        Generate vector
-        Vector<Long> vector = new Vector<>(sizeOfVector);                        // Create 'Vector' instead of array
-        for (int i = 0; i < sizeOfVector; i++) {
-            vector.add(ThreadLocalRandom.current().nextLong(1_0));        // Filling vector pseudo-random numbers
+            Vector<Long> vector = new Vector<>(sizeOfVector);                        // Create 'Vector' instead of array
+            for (int i = 0; i < sizeOfVector; i++) {
+                vector.add(ThreadLocalRandom.current().nextLong(0, 100_000));        // Filling vector pseudo-random numbers
+            }
+
+            System.out.println("Size of array: " + sizeOfVector + ",\t" + vector);   // Display to console
+
+            long result_1 = MaxPairwiseProduct(sizeOfVector, vector);
+            long result_2 = MaxPairwiseProductFast(sizeOfVector, vector);
+
+            System.out.println("№: " + step + "|\tFunc._1 = " + result_1 + "\t\tFunc._2 = " + result_2);
+
+            if (result_1 != result_2) {
+                System.err.println("Wrong answer: " + result_1 + " & " + result_2);
+                break;
+            } else {
+                System.out.println("ok");
+            }
+            step += 1;
         }
-
-
-        System.out.println("Maximum two numbers from 'Function' 1:\t" + MaxPairwiseProduct(sizeOfVector, vector));
-        System.out.println("Maximum two numbers from 'Function' 2:\t" + MaxPairwiseProductFast(sizeOfVector, vector));
 
     }
 }
