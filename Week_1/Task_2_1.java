b8d16004d23f095de15e33e7f220bf6e5f27348f
EfimovAlNi <EfimovAlNi@ya.ru>
2021-02-21 02:03:54 +0100

    All is work on my computer, but server can't submit this work. Write this error: Your output:
    Your stderr:
    Exception in thread "main" java.util.NoSuchElementException
    	at java.util.Scanner.throwFor(Scanner.java:862)
    	at java.util.Scanner.next(Scanner.java:1485)
    	at java.util.Scanner.nextLong(Scanner.java:2222)
    	at java.util.Scanner.nextLong(Scanner.java:2182)
    	at Task_2_1.main(Task_2_1.java:68)
    
    Correct output:
    6
     (Time used: 0.08/1.50, memory used: 25690112/536870912.)

--- Week_1/Task_2_1.java
+++ Week_1/Task_2_1.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
 import java.util.Vector;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Task_2_1 {
+    //    Method which get size of vector and generate vector.
+    public static Vector<Long> VectorOur(int sizeOfVector) {
+        Vector<Long> vector = new Vector<>(sizeOfVector);                        // Create 'Vector' instead of array
+        for (int i = 0; i < sizeOfVector; i++) {
+            vector.add(ThreadLocalRandom.current().nextLong(0, 10_00_000_000));        // Filling vector pseudo-random numbers, 'bound' - max number
+        }
+        return vector;
+    }
 
+    //  Method '1' which get size of vector and vector & find two maximum numbers
     public static long MaxPairwiseProduct(int sizeOfVector, Vector<Long> vector) {
         long result = 0;
         long max_1 = 0;
@@ -22,7 +32,7 @@ public class Task_2_1 {
         return result;
     }
 
-
+    //  Method '2' which get size of vector and vector & find two maximum numbers
     public static long MaxPairwiseProductFast(int sizeOfVector, Vector<Long> vector) {
         long max_1 = 0;
         long max_2 = 0;
@@ -40,29 +50,53 @@ public class Task_2_1 {
                 max_2 = vector.get(i);
             }
         }
-
         return max_2 * max_1;
     }
 
 
     public static void main(String[] args) {
-        int max = 13;
-        int min = 2;
+        //  *** Manual input data start *** ----------> for using 'Stress test', need to comment this block of code!!!
+
+         Scanner scanner = new Scanner(System.in);
+         int sizeOfVector = scanner.nextInt();               // Generate size of vector
+
+        Vector<Long> vectorManual = new Vector<Long>();
+
+        //  Filling the vector
+        Scanner numbersOfVector = new Scanner(System.in);
+        for (int i = 0; i < sizeOfVector; i++) {
+            vectorManual.add(numbersOfVector.nextLong());
+        }
+
+        scanner.close();
+        numbersOfVector.close();
+
+
+         long result_1 = MaxPairwiseProduct(sizeOfVector, vectorManual);
+        long result_2 = MaxPairwiseProductFast(sizeOfVector, vectorManual);
+
+
+         System.out.println("Func._1 = " + result_1 + "\t\tFunc._2 = " + result_2);   // For output solution of two 'Methods'
+        System.out.println(result_2);
+        //  *** Manual input data end ***
+
+
+        //        Stress-test, for using uncomment this block  of code!
+        /**
+        //  *** Stress test start ***
+        int max = 1_000;  // Max length of vector
+        int min = 2;        // Min length of vector
         int step = 0;
 
         while (true) {
             int sizeOfVector = (int) ((Math.random() * (max - min)) + min);         // Generate & calculate numbers of interval [2..13]
 
-//        Generate vector
-            Vector<Long> vector = new Vector<>(sizeOfVector);                        // Create 'Vector' instead of array
-            for (int i = 0; i < sizeOfVector; i++) {
-                vector.add(ThreadLocalRandom.current().nextLong(0, 100_000));        // Filling vector pseudo-random numbers
-            }
+            Vector<Long> vectorManual = (Vector<Long>) VectorOur(sizeOfVector);
 
-            System.out.println("Size of array: " + sizeOfVector + ",\t" + vector);   // Display to console
+            System.out.println("Size of array: " + sizeOfVector + ",\t" + vectorManual);   // Display to console
 
-            long result_1 = MaxPairwiseProduct(sizeOfVector, vector);
-            long result_2 = MaxPairwiseProductFast(sizeOfVector, vector);
+            long result_1 = MaxPairwiseProduct(sizeOfVector, vectorManual);
+            long result_2 = MaxPairwiseProductFast(sizeOfVector, vectorManual);
 
             System.out.println("№: " + step + "|\tFunc._1 = " + result_1 + "\t\tFunc._2 = " + result_2);
 
@@ -74,6 +108,7 @@ public class Task_2_1 {
             }
             step += 1;
         }
-
+        //  *** Stress test finish! ***
+        */
     }
 }
